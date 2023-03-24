2a8deed5b8b9cad5290f08f9a416e0c2d71a31e3
Ihor Kopieichykov <igor129k02@gmail.com>
2023-03-24 17:39:42 +0200

    adding reusable components

--- src/components/TalentsListPage/SearchPanel/SearchPanel.jsx
+++ src/components/TalentsListPage/SearchPanel/SearchPanel.jsx
@@ -1,25 +1,13 @@
-import styles from "./SearchPanel.module.scss";
+import { Button, Input } from "../../../shared/components";
+import s from "./SearchPanel.module.scss";
 
 export function SearchPanel() {
     return (
-        <div className={styles["search-panel"]}>
-            <input
-                type="text"
-                className={styles["search-input1"]}
-                placeholder="Keywords/job title"
-            />
-            <input
-                type="text"
-                className={styles["search-input"]}
-                placeholder="Location"
-            />
-            <input
-                type="text"
-                className={styles["search-input"]}
-                placeholder="Distance"
-            />
-
-            <button className={styles.btn}>Search</button>
+        <div className={s["search-panel"]}>
+            <Input id={s.long} className={s.input} type="text" placeholder="Name"/>
+            <Input className={s.input} type="text" placeholder="Specialization"/>
+            <Input className={s.input} type="text" placeholder="Skill"/>
+            <Button className={s.btn}>Search</Button>
         </div>
     );
 }

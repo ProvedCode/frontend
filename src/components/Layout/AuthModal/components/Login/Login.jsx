4f47cd2719dd693cabc376255b07f33a85e4d871
Ruslanchik01 <morozov2020ks11@student.karazin.ua>
2023-03-30 22:15:57 +0300

    added validation (hardcode)

--- src/components/Layout/AuthModal/components/Login/Login.jsx
+++ src/components/Layout/AuthModal/components/Login/Login.jsx
@@ -1,55 +1,92 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../../../context/UserContext/UserContext";
 import { Button, Input } from "../../../../../shared/components";
 import s from "./Login.module.scss";
 
 const advices = [
-	{
-		title: "Searching and connecting",
-		text: "You can search for the talents all around the globe and connect with them easily",
-	},
-	{
-		title: "Upload any quantity of proofs",
-		text: "It is allowed for you to upload any quantity of proofs you want.",
-	},
-	{
-		title: "Job alerts",
-		text: " You will be immediately recognised, when employers will want to make connection.",
-	},
-	{
-		title: "Free CV review",
-		text: "It is allowed to have the CV review for free!",
-	},
+    {
+        title: "Searching and connecting",
+        text: "You can search for the talents all around the globe and connect with them easily",
+    },
+    {
+        title: "Upload any quantity of proofs",
+        text: "It is allowed for you to upload any quantity of proofs you want.",
+    },
+    {
+        title: "Job alerts",
+        text: " You will be immediately recognised, when employers will want to make connection.",
+    },
+    {
+        title: "Free CV review",
+        text: "It is allowed to have the CV review for free!",
+    },
 ];
 
 export function Login() {
-	const { setAuth } = useContext(UserContext);
-	const location = useLocation();
-	const navigate = useNavigate();
-	return (
-		<>
-			<form action="#" className={s.form}>
-				<Input type="text" className={s.input}></Input>
-				<Input type="text" className={s.input}></Input>
-				<Button type="submit" className={s.btn}>
-					Login
-				</Button>
-			</form>
-			<div className={s.info}>
-				<div className={s.title}>Not Registered?</div>
-				<ul className={s.advices}>
-					{advices.map(({title, text}) => (
-						<li key={title}>
-							<h4>{title}</h4>
-							<p>
-								{text}
-							</p>
-						</li>
-					))}
-				</ul>
-				<Button className={s.btn} onClick={()=>navigate(location.state.redirect, {replace: true})}>Register</Button>
-			</div>
-		</>
-	);
+    const { setAuth } = useContext(UserContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	// const [validEmail, setValidEmail] = useState(true);
+	// const [validPassword, setValidPassword] = useState(true);
+
+	function validateEmail() {
+		const EMAIL_REGEXP  = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		return EMAIL_REGEXP.test(String(email).toLowerCase());
+	}
+	function validatePassword() {
+		const PASSWORD_REGEXP  = /^[a-zA-Z0-9]{8,}$/;
+		return PASSWORD_REGEXP.test(String(password).toLowerCase());
+	}
+	function validateForm(){
+		// validateEmail(email);
+		// validatePassword(password);
+		return validateEmail() && validatePassword();
+	}
+	
+	function handler(event){
+		event.preventDefault()
+	}
+    return (
+        <>
+            <form action="#" onSubmit={handler} className={s.form}>
+                <div className={s.input_block}>
+                    <label htmlFor="login">Login</label>
+                    <Input name="login" type="text" required placeholder="example@gmail.com" className={`${s.input} ${validateEmail() ? "" : s.error}`} onChange={event => setEmail(event.target.value)}></Input>
+                    <span>{validateEmail() ? "" : "*not valid email"}</span>
+                </div>
+                <div className={s.input_block}>
+                    <label htmlFor="password">Password</label>
+                    <Input name="password" type="text" required placeholder="your password" className={`${s.input} ${validatePassword() ? "" : s.error}`} onChange={event => setPassword(event.target.value)}></Input>
+					<span>{validatePassword() ? "" : "*not valid password"}</span>
+                </div>
+                <Button type="submit" className={s.btn} onClick={()=>validateForm()}>
+                    Login
+                </Button>
+            </form>
+            <div className={s.info}>
+                <div className={s.title}>Not Registered?</div>
+                <ul className={s.advices}>
+                    {advices.map(({ title, text }) => (
+                        <li key={title}>
+                            <h4>{title}</h4>
+                            <p>{text}</p>
+                        </li>
+                    ))}
+                </ul>
+                <Button
+                    className={s.btn}
+                    onClick={() =>
+                        navigate(location.state.redirect, { replace: true })
+                    }
+                >
+                    Register
+                </Button>
+            </div>
+        </>
+    );
 }
--- src/components/Layout/AuthModal/components/Register/Register.jsx
+++ src/components/Layout/AuthModal/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Input } from "../../../../../shared/components";
 import s from "./Register.module.scss";
 
@@ -22,14 +22,128 @@ const advices = [
 ];
 
 export function Register() {
+
+	const [firstName, SetFirstName] = useState("");
+	const [lastName, SetLastName] = useState("");
+	const [specialization, setSpecialization] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [repeatPassword, setRepeatPassword] = useState("");
+
+
+	const [validFirstName, setValidFirstName] = useState(true);
+	const [validLastName, setValidLastName] = useState(true);
+	const [validSpec, setValidSpec] = useState(true);
+	const [validEmail, setValidEmail] = useState(true);
+	const [validPassword, setValidPassword] = useState(true);
+	const [validRepeatPassword, setValidRepeatPassword] = useState(true);
+
+	function validateFirstName (fname){
+		const FIRST_NAME_REGEXP  = /^[a-zA-Z\s]{1,30}$/;
+		if(FIRST_NAME_REGEXP.test(String(fname).toLowerCase())){
+			setValidFirstName(true);
+		}
+		else{
+			setValidFirstName(false);
+		}
+	}
+
+	function validateLastName (lname){
+		const LAST_NAME_REGEXP  = /^[a-zA-Z\s]{1,30}$/;
+		if(LAST_NAME_REGEXP.test(String(lname).toLowerCase())){
+			setValidLastName(true);
+		}
+		else{
+			setValidLastName(false);
+		}
+	}
+
+	function validateSpecialization (spec){
+		const SPECIALIZATION_REGEXP  = /^[a-zA-Z\s]{1,50}$/;
+		if(SPECIALIZATION_REGEXP.test(String(spec).toLowerCase())){
+			setValidSpec(true);
+		}
+		else{
+			setValidSpec(false);
+		}
+	}
+
+	function validateEmail(email) {
+		const EMAIL_REGEXP  = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		if(EMAIL_REGEXP.test(String(email).toLowerCase())){
+			setValidEmail(true);
+		}
+		else{
+			setValidEmail(false);
+		}
+	}
+
+	function validatePassword(password) {
+		const PASSWORD_REGEXP  = /^[a-zA-Z0-9]{8,}$/;
+		if(PASSWORD_REGEXP.test(String(password).toLowerCase())){
+			setValidPassword(true);
+		}
+		else{
+			setValidPassword(false);
+		}
+	}
+
+	function validateRepeatPassword(password, repPassword) {
+		if(password === repPassword){
+			setValidRepeatPassword(true);
+		}
+		else{
+			setValidRepeatPassword(false);
+		}
+	}
+
+	function valedateAll(fname, lname, spec, email, password, repPassword){
+		validateFirstName(fname);
+		validateLastName(lname);
+		validateSpecialization(spec);
+		validateEmail(email);
+		validatePassword(password);
+		validateRepeatPassword(password, repPassword);
+	}
+
+	function handler(event){
+		event.preventDefault()
+	}
 	return (
 		<>
-			<form action="#" className={s.form}>
-				<Input type="text" className={s.input}></Input>
-				<Input type="text" className={s.input}></Input>
-				<Input type="text" className={s.input}></Input>
-				<Input type="text" className={s.input}></Input>
-				<Button type="submit" className={s.btn}>
+			<form action="#" onSubmit={handler} className={s.form}>
+				<div className={s.input_block}>
+                    <label htmlFor="first_name">First Name</label>
+                    <Input name="first_name" type="text" required placeholder="John" autoComplete="off" className={`${s.input} ${validFirstName ? "" : s.error}`} onChange={event => SetFirstName(event.target.value)}></Input>
+                    <span>{validFirstName ? "" : "*not valid first name"}</span>
+                </div>
+				<div className={s.input_block}>
+                    <label htmlFor="last_name">Last Name</label>
+                    <Input name="last_name" type="text" required placeholder="Brown" className={`${s.input} ${validLastName ? "" : s.error}`} onChange={event => SetLastName(event.target.value)}></Input>
+                    <span>{validLastName ? "" : "*not valid last name"}</span>
+                </div>
+				<div className={s.input_block}>
+                    <label htmlFor="login">Login</label>
+                    <Input name="login" type="text" required placeholder="example@gmail.com" className={`${s.input} ${validEmail ? "" : s.error}`} onChange={event => setEmail(event.target.value)}></Input>
+                    <span>{validEmail ? "" : "*not valid email"}</span>
+                </div>
+				<div className={s.input_block}>
+                    <label htmlFor="specialization">Specialization</label>
+                    <Input name="specialization" type="text" required placeholder="Java Developer" className={`${s.input} ${validSpec ? "" : s.error}`} onChange={event => setSpecialization(event.target.value)}></Input>
+                    <span>{validSpec ? "" : "*not valid specialization"}</span>
+                </div>
+				<div className={s.input_block}>
+                    <label htmlFor="password">Password</label>
+                    <Input name="password" type="text" required placeholder="your password" className={`${s.input} ${validPassword ? "" : s.error}`} onChange={event => setPassword(event.target.value)}></Input>
+                    <span>{validPassword ? "" : "*not valid password"}</span>
+                </div>
+				<div className={s.input_block}>
+                    <label htmlFor="repeated_password">Repeat Password</label>
+                    <Input name="repeated_password" type="text" required placeholder="repeat password" className={`${s.input} ${validRepeatPassword ? "" : s.error}`} onChange={event => setRepeatPassword(event.target.value)}></Input>
+                    <span>{validRepeatPassword ? "" : "*password repeat incorrectly"}</span>
+                </div>
+				
+				<Button type="submit" className={s.btn} onClick={()=>valedateAll(firstName, lastName,specialization, email, password, repeatPassword )}>
 					Register
 				</Button>
 			</form>
